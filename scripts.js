15ae1f7959c06e37d1d5aedbe554eb190c2e673f
KMTWIFI <116014391+KMTWIFI@users.noreply.github.com>
2023-05-08 21:06:55 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 // scripts.js
 $(document).ready(function () {
+    const modal = document.getElementById("myModal");
+    const closeBtn = document.getElementsByClassName("close")[0];
+
     const swiper = new Swiper('.swiper-container', {
         allowTouchMove: false,
         noSwiping: true,
@@ -15,11 +18,9 @@ $(document).ready(function () {
         window.location.href = 'http://kmt-group.instawp.xyz/';
     });
 
-    $('#yesBtn').on('click', function () {
-        $('.question-container').hide();
-        $('.video-container').hide();
-        $('.form-container').show();
-        $('.rounded-box2').show();
+$('#yesBtn').on('click', function () {
+  modal.style.display = "block";
+
 
         // Initialize intl-tel-input if it has not been initialized yet
         if (!iti) {
@@ -30,11 +31,21 @@ $(document).ready(function () {
                  initialCountry: "eg",
             });
         }
+        closeBtn.onclick = function () {
+          modal.style.display = "none";
+        };
+
+        window.onclick = function (event) {
+          if (event.target === modal) {
+            modal.style.display = "none";
+  }}
     });
 
     $('#noBtn').on('click', function () {
         window.location.href = 'http://kmt-group.instawp.xyz/';
     });
+    
+
 
     $('form').on('submit', function (event) {
         event.preventDefault();
@@ -89,3 +100,67 @@ function updateHintMessage(remainingTime) {
     const timeText = remainingTime === 1 ? 'ثانية' : 'ثواني';
     hintMessage.innerHTML = `يمكنك المتابعة في خلال ${remainingTime} ${timeText}`;
 }
+
+function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
+}
+
+function updateDays() {
+  const daySelect = document.getElementById('day');
+  const monthSelect = document.getElementById('month');
+  const yearSelect = document.getElementById('year');
+
+  const month = parseInt(monthSelect.value, 10);
+  const year = parseInt(yearSelect.value, 10);
+
+  if (isNaN(month) || isNaN(year)) {
+    return;
+  }
+
+  const days = daysInMonth(month, year);
+  const currentDay = parseInt(daySelect.value, 10);
+
+  daySelect.innerHTML = '<option value="">يوم</option>';
+
+  for (let i = 1; i <= days; i++) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = i;
+    daySelect.appendChild(option);
+  }
+
+  if (currentDay > 0 && currentDay <= days) {
+    daySelect.value = currentDay;
+  }
+}
+
+// Add the updateDays function to the populateMonths function
+function populateMonths() {
+  const monthSelect = document.getElementById('month');
+  for (let i = 1; i <= 12; i++) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = i;
+    monthSelect.appendChild(option);
+  }
+  monthSelect.addEventListener('change', updateDays);
+}
+
+// Add the updateDays function to the populateYears function
+function populateYears(startYear) {
+  const yearSelect = document.getElementById('year');
+  const currentYear = new Date().getFullYear();
+  for (let i = startYear; i <= currentYear; i++) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = i;
+    yearSelect.appendChild(option);
+  }
+  yearSelect.addEventListener('change', updateDays);
+}
+
+
+// Populate the date dropdowns
+populateMonths();
+populateYears(1950);
+updateDays();
